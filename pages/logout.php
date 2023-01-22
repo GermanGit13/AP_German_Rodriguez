ba18023d59018225468d6e0f5288dce4c421b8f8
GermanGit13 <99618445+GermanGit13@users.noreply.github.com>
2023-01-22 23:15:33 +0100

    Sesiones

--- pages/logout.php
+++ pages/logout.php
@@ -2,5 +2,5 @@
 session_start(); //recuperamos la sesion
 $_SESSION=array(); //asignamos al array _SESSION UN ARRAY VACIO
 session_destroy(); //PARA BORRAR TODOS LOS DATOS DE LA SESION
-//header("Location: ../index.php");
+header("Location: ../index.php");
 ?>
